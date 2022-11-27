fc6ef93554dd72bb8f53f6eb02820211e682c612
Leandro Rezende Vieira <leandrorvieira22@yahoo.com>
2022-11-27 06:29:58 -0300

    user gesture

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Routes } from './src/routes';
 import theme from './src/styles/theme';
-import { ThemeProvider } from 'styled-components'; 
+import { ThemeProvider } from 'styled-components';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 import {
   useFonts,
   Inter_400Regular,
   Inter_500Medium
 } from '@expo-google-fonts/inter';
+
 import {
   Archivo_400Regular,
   Archivo_500Medium,
@@ -23,14 +26,16 @@ export default function App() {
     Archivo_600SemiBold
   });
 
-  if(!fontsLoaded){
+  if (!fontsLoaded) {
     return null;
-  }
+  };
+
   return (
-    <ThemeProvider theme={theme}>        
-      <Routes />
+    <ThemeProvider theme={theme}>
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <Routes />
+      </GestureHandlerRootView>
     </ThemeProvider>
-    
-  )  
+  )
 }
 
--- src/Screens/Home/index.tsx
+++ src/Screens/Home/index.tsx
@@ -1,5 +1,4 @@
 import { api } from '../../services/api';
-import { StatusBar } from 'react-native';
 import Logo from '../../assets/logo.svg';
 import { CarDTO } from '../../dtos/CarDTO';
 import { Car } from '../../components/Car';
@@ -9,6 +8,16 @@ import { Ionicons } from '@expo/vector-icons';
 import React, { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import { RFValue } from 'react-native-responsive-fontsize';
+import { PanGestureHandler } from 'react-native-gesture-handler';
+import { StatusBar, TouchableOpacity, StyleSheet } from 'react-native';
+
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  useAnimatedGestureHandler,
+  withSpring
+} from 'react-native-reanimated';
+
 import {
   Container,
   Header,
@@ -27,11 +36,40 @@ interface NavigationProps {
   ) => void
 }
 
+const ButtonAnimated = Animated.createAnimatedComponent(TouchableOpacity);
+
 export function Home() {
-  const [cars, setCars] = useState<CarDTO[]>([]);
-  const [loading, setLoading] = useState(true);
-  const navigation = useNavigation<NavigationProps>();
   const theme = useTheme();
+
+  const positionY = useSharedValue(0);
+  const positionX = useSharedValue(0);
+  const myCarsButtonStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: positionX.value },
+        { translateY: positionY.value }
+      ]
+    }
+  });
+
+  const onGestureEvent = useAnimatedGestureHandler({
+    onStart(_, ctx: any){
+      ctx.positionX = positionX.value;
+      ctx.positionY = positionY.value;	
+    },
+    onActive(event, ctx: any){
+      positionX.value = ctx.positionX + event.translationX;
+      positionY.value = ctx.positionY + event.translationY;
+    },
+    onEnd(ctx: any){
+      positionX.value = withSpring(0);
+      positionY.value = withSpring(0);
+    }
+  });
+
+  const [loading, setLoading] = useState(true);
+  const [cars, setCars] = useState<CarDTO[]>([]);
+  const navigation = useNavigation<NavigationProps>();
 
   function handleCarDetails(car: CarDTO) {
     navigation.navigate('CarDetails', { car })
@@ -80,13 +118,39 @@ export function Home() {
             <Car data={item} onPress={() => handleCarDetails(item)} />}
         />
       }
-      <MyCarsButton onPress={handleOpenMyCars}>
-        <Ionicons
-          name="ios-car-sport"
-          size={32}
-          color={theme.colors.shape}
-        />
-      </MyCarsButton>
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <Animated.View
+          style={[
+            myCarsButtonStyle,
+            {
+              position: 'absolute',
+              bottom: 13,
+              right: 22,
+            }
+          ]}
+        >
+          <ButtonAnimated
+            onPress={handleOpenMyCars}
+            style={[styles.button, { backgroundColor: theme.colors.main }]}
+          >
+            <Ionicons
+              name="ios-car-sport"
+              size={32}
+              color={theme.colors.shape}
+            />
+          </ButtonAnimated>
+        </Animated.View>
+      </PanGestureHandler>
     </Container>
   )
 }
+
+const styles = StyleSheet.create({
+  button: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
